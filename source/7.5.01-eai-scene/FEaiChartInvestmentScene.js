186c80c910214ec9acd18d7c5d64ef4acfeb0c90
AzureV <AzureV@outlook.com>
2015-06-22 17:39:20 +0800

    投资历史按日更新表格显示各省数据
    Conflicts:
    	source/7.5.01-eai-scene/FEaiChartInvestmentScene.js

--- source/7.5.01-eai-scene/FEaiChartInvestmentScene.js
+++ source/7.5.01-eai-scene/FEaiChartInvestmentScene.js
@@ -5,27 +5,29 @@
 // @author maocy
 // @history 150618
 //==========================================================
-MO.FEaiChartInvestmentScene = function FEaiChartInvestmentScene(o){
+MO.FEaiChartInvestmentScene = function FEaiChartInvestmentScene(o) {
    o = MO.RClass.inherits(this, o, MO.FEaiChartScene);
    //..........................................................
    // @attribute
-   o._code        = MO.EEaiScene.ChartInvestment;
-   o._playing     = false;
-   o._startDate   = null;
-   o._endDate     = null;
+   o._code = MO.EEaiScene.ChartInvestment;
+   o._playing = false;
+   o._startDate = null;
+   o._endDate = null;
    o._currentDate = null;
+   o._currentRow = 0;
+   o._lastDateRowCount = 0;
    //..........................................................
    // @event
-   o.onLoadData   = MO.FEaiChartInvestmentScene_onLoadData;
-   o.onKeyDown    = MO.FEaiChartInvestmentScene_onKeyDown;
+   o.onLoadData = MO.FEaiChartInvestmentScene_onLoadData;
+   o.onKeyDown = MO.FEaiChartInvestmentScene_onKeyDown;
    //..........................................................
    // @method
-   o.setup        = MO.FEaiChartInvestmentScene_setup;
-   o.selectDate   = MO.FEaiChartInvestmentScene_selectDate;
+   o.setup = MO.FEaiChartInvestmentScene_setup;
+   o.selectDate = MO.FEaiChartInvestmentScene_selectDate;
    // @method
-   o.active       = MO.FEaiChartInvestmentScene_active;
-   o.process      = MO.FEaiChartInvestmentScene_process;
-   o.deactive     = MO.FEaiChartInvestmentScene_deactive;
+   o.active = MO.FEaiChartInvestmentScene_active;
+   o.process = MO.FEaiChartInvestmentScene_process;
+   o.deactive = MO.FEaiChartInvestmentScene_deactive;
    return o;
 }
 
@@ -35,7 +37,7 @@ MO.FEaiChartInvestmentScene = function FEaiChartInvestmentScene(o){
 // @method
 // @param event:SEvent 事件信息
 //==========================================================
-MO.FEaiChartInvestmentScene_onLoadData = function FEaiChartInvestmentScene_onLoadData(event){
+MO.FEaiChartInvestmentScene_onLoadData = function FEaiChartInvestmentScene_onLoadData(event) {
    var o = this;
    o.__base.FEaiChartScene.onLoadData.call(o, event);
    var code = o._currentDate.format('YYYYMMDD')
@@ -48,22 +50,22 @@ MO.FEaiChartInvestmentScene_onLoadData = function FEaiChartInvestmentScene_onLoa
 // @method
 // @param event:SEvent 事件信息
 //==========================================================
-MO.FEaiChartInvestmentScene_onKeyDown = function FEaiChartInvestmentScene_onKeyDown(event){
+MO.FEaiChartInvestmentScene_onKeyDown = function FEaiChartInvestmentScene_onKeyDown(event) {
    var o = this;
    var keyCode = event.keyCode;
-   if(keyCode == MO.EKeyCode.N){
+   if (keyCode == MO.EKeyCode.N) {
       o._currentDate.addDay(-1);
       var code = o._currentDate.format('YYYYMMDD')
       o.selectDate(code);
       console.log(code);
    }
-   if(keyCode == MO.EKeyCode.M){
+   if (keyCode == MO.EKeyCode.M) {
       o._currentDate.addDay(1);
       var code = o._currentDate.format('YYYYMMDD')
       o.selectDate(code);
       console.log(code);
    }
-   if(keyCode == MO.EKeyCode.L){
+   if (keyCode == MO.EKeyCode.L) {
       MO.RDate.autoParse(o._currentDate, '20140701');
       o._playing = true;
    }
@@ -75,7 +77,7 @@ MO.FEaiChartInvestmentScene_onKeyDown = function FEaiChartInvestmentScene_onKeyD
 // @method
 // @param event:SEvent 事件信息
 //==========================================================
-MO.FEaiChartInvestmentScene_selectDate = function FEaiChartInvestmentScene_selectDate(code){
+MO.FEaiChartInvestmentScene_selectDate = function FEaiChartInvestmentScene_selectDate(code) {
    var o = this;
    // 构建画面
    var context = o.graphicContext();
@@ -85,20 +87,34 @@ MO.FEaiChartInvestmentScene_selectDate = function FEaiChartInvestmentScene_selec
    var dataLayer = stage.dataLayer();
    var historyConsole = MO.Console.find(MO.FEaiResourceConsole).historyConsole();
    var dateData = historyConsole.dates().get(code);
-   if(dateData){
+   if (dateData) {
       // 设置省份数据
       var provincesData = dateData.provinces();
       var count = provincesData.count();
-      for(var i = 0; i < count; i++){
+      for (var i = 0; i < count; i++) {
          var provinceData = provincesData.at(i);
          var provinceEntity = o._provinceEntities.get(provinceData.code());
          provinceEntity.update(provinceData);
       }
+      //控制表格行显隐
+      var invesTable = document.getElementById('id_investment_table');
+      for (var i = 0; i < o._lastDateRowCount; i++) {
+         var row = invesTable.rows[o._currentRow - i];
+         row.style.display = 'none';
+      }
+      for (var i = 0; i < count; i++) {
+         //rows[0]为标题栏，这里需要+1
+         var row = invesTable.rows[o._currentRow + 1 + i];
+         row.style.display = '';
+      }
+      o._currentRow += count;
+      o._lastDateRowCount = count;
+      
       // 设置城市数据
       var cityDatas = dateData.citys();
       var cityEntities = o._cityEntities;
       var count = cityEntities.count();
-      for(var i = 0; i < count; i++){
+      for (var i = 0; i < count; i++) {
          var cityEntity = cityEntities.at(i);
          var code = cityEntity.data().code();
          var data = cityDatas.get(code);
@@ -117,7 +133,7 @@ MO.FEaiChartInvestmentScene_selectDate = function FEaiChartInvestmentScene_selec
 //
 // @method
 //==========================================================
-MO.FEaiChartInvestmentScene_setup = function FEaiChartInvestmentScene_setup(){
+MO.FEaiChartInvestmentScene_setup = function FEaiChartInvestmentScene_setup() {
    var o = this;
    o.__base.FEaiChartScene.setup.call(o);
    MO.RWindow.lsnsKeyDown.register(o, o.onKeyDown);
@@ -127,6 +143,34 @@ MO.FEaiChartInvestmentScene_setup = function FEaiChartInvestmentScene_setup(){
    o._currentDate.parseAuto('20140701');
    o._startDate.parseAuto('20140701');
    o._endDate.parseAuto('20150618');
+   //填充表格
+   var historyConsole = MO.Console.find(MO.FEaiResourceConsole).historyConsole();
+   var provinceConsole = MO.Console.find(MO.FEaiResourceConsole).provinceConsole();
+   var invesTable = document.getElementById('id_investment_table');
+   var currentDate = o._currentDate;
+   while (true) {
+      var dateData = historyConsole.dates().get(currentDate.format('YYYYMMDD'));
+      if (dateData) {
+         // 设置省份数据
+         var provincesData = dateData.provinces();
+         var count = provincesData.count();
+         for (var i = 0; i < count; i++) {
+            var provinceInvesData = provincesData.at(i);
+            var provinceResData = provinceConsole.findByCode(provinceInvesData.code());
+            var row = invesTable.insertRow(invesTable.rows.length);
+            var labelCol = row.insertCell(0);
+            var invesCol = row.insertCell(1);
+            labelCol.innerHTML = provinceResData.label();
+            invesCol.innerHTML = provinceInvesData.investmentTotal();
+            row.style.display = 'none';
+         }
+         currentDate.addDay(1);
+      }
+      else {
+         break;
+      }
+   }
+   o._currentDate.parseAuto('20140701');
 }
 
 //==========================================================
@@ -134,7 +178,7 @@ MO.FEaiChartInvestmentScene_setup = function FEaiChartInvestmentScene_setup(){
 //
 // @method
 //==========================================================
-MO.FEaiChartInvestmentScene_active = function FEaiChartInvestmentScene_active(){
+MO.FEaiChartInvestmentScene_active = function FEaiChartInvestmentScene_active() {
    var o = this;
    o.__base.FEaiChartScene.active.call(o);
 }
@@ -144,15 +188,15 @@ MO.FEaiChartInvestmentScene_active = function FEaiChartInvestmentScene_active(){
 //
 // @method
 //==========================================================
-MO.FEaiChartInvestmentScene_process = function FEaiChartInvestmentScene_process(){
+MO.FEaiChartInvestmentScene_process = function FEaiChartInvestmentScene_process() {
    var o = this;
    o.__base.FEaiChartScene.process.call(o);
-   if(o._playing){
+   if (o._playing) {
       o._currentDate.addDay(1);
       var code = o._currentDate.format('YYYYMMDD')
       var endCode = o._endDate.format('YYYYMMDD')
       o.selectDate(code);
-      if(code == endCode){
+      if (code == endCode) {
          o._playing = false;
       }
    }
@@ -163,7 +207,7 @@ MO.FEaiChartInvestmentScene_process = function FEaiChartInvestmentScene_process(
 //
 // @method
 //==========================================================
-MO.FEaiChartInvestmentScene_deactive = function FEaiChartInvestmentScene_deactive(){
+MO.FEaiChartInvestmentScene_deactive = function FEaiChartInvestmentScene_deactive() {
    var o = this;
    o.__base.FEaiChartScene.deactive.call(o);
 }
