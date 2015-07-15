d545966cb37e5d4f7d0f150d9f705c9abca490cd
sunhuairui <sunrui908@163.com>
2015-07-15 09:17:21 +0800

    [底层控件] 绘制抛锚矩形（还没有做完）

--- source/4.5.06-gui-extension/FGuiDesignAnchor.js
+++ source/4.5.06-gui-extension/FGuiDesignAnchor.js
@@ -5,13 +5,25 @@
 // @author maocy
 // @version 150714
 //==========================================================
-MO.FGuiDesignAnchor = function FGuiDesignAnchor(o){
-   o = MO.Class.inherits(this, o, MO.FGuiControl);
-   // @method
-   o.construct = MO.FGuiDesignAnchor_construct;
-   // @method
-   o.dispose   = MO.FGuiDesignAnchor_dispose;
-   return o;
+MO.FGuiDesignAnchor = function FGuiDesignAnchor(o) {
+    o = MO.Class.inherits(this, o, MO.FGuiControl);
+    // @method
+    o.construct = MO.FGuiDesignAnchor_construct;
+    o.onPaintBegin = FGuiDesignAnchor_onPaintBegin;
+
+    // @method
+    o.dispose = MO.FGuiDesignAnchor_dispose;
+    return o;
+}
+
+//==========================================================
+// <T>。</T>
+//
+// @method
+//==========================================================
+MO.FGuiDesignAnchor_construct = function FGuiDesignAnchor_construct() {
+    var o = this;
+    o.__base.FGuiControl.construct.call(o);
 }
 
 //==========================================================
@@ -19,9 +31,12 @@ MO.FGuiDesignAnchor = function FGuiDesignAnchor(o){
 //
 // @method
 //==========================================================
-MO.FGuiDesignAnchor_construct = function FGuiDesignAnchor_construct(){
-   var o = this;
-   o.__base.FGuiControl.construct.call(o);
+MO.FGuiDesignAnchor_onPaintBegin = function FGuiDesignAnchor_onPaintBegin(event){
+    var o = this;
+    o.__base.FGuiControl.onPaintBegin.call(o,event);
+    var graphic = event.graphic;
+    var rectangle = event.rectangle;
+    //
 }
 
 //==========================================================
@@ -29,8 +44,8 @@ MO.FGuiDesignAnchor_construct = function FGuiDesignAnchor_construct(){
 //
 // @method
 //==========================================================
-MO.FGuiDesignAnchor_dispose = function FGuiDesignAnchor_dispose(){
-   var o = this;
-   // 父处理
-   o.__base.FGuiControl.dispose.call(o);
+MO.FGuiDesignAnchor_dispose = function FGuiDesignAnchor_dispose() {
+    var o = this;
+    // 父处理
+    o.__base.FGuiControl.dispose.call(o);
 }
