b0ec13085afd87db4b542acf1562f9af22fc696a
zyj2575 <zhaoyaoji2575@163.com>
2015-07-28 15:33:27 +0800

    requesttime  更改为 tick

--- source/7.1.03-eai-console/FEaiLogic.js
+++ source/7.1.03-eai-console/FEaiLogic.js
@@ -53,7 +53,7 @@ with(MO){
             for(var i=0; i < addKey(5,3).length; i++){
                key+=addKey(5,3)[i];
             }
-            url += '?' + parameters+"&requesttime="+time+"&sign="+hex_md5(ginsengs+key);
+            url += '?' + parameters+"&tick="+time+"&sign="+hex_md5(ginsengs+key);
          }
       return url;
    } 
