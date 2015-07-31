1fe86323d9cbdb618e4a36a34254019bae9a78bd
sunhuairui <sunrui908@163.com>
2015-07-31 18:37:23 +0800

    理财师UI控件

--- source/7.2.01-eai-entity-country/FGuiFPCCTable.js
+++ source/7.2.01-eai-entity-country/FGuiFPCCTable.js
@@ -63,28 +63,45 @@ MO.FGuiFPCCTable_onPaintBegin = function FGuiFPCCTable_onPaintBegin(event) {
    var top = rectangle.top;
    var height = rectangle.height;
    var width = rectangle.width;
-   var padding = 5;
+   var padding = 6;
    //列表矩形
    graphic.drawRectangle(left, top, width, height, "#fff", 1);
    //标题
    var titleText = "理财师业绩展示中心";
-   var titleHeight = 32;
+   var titleHeight = 30;
    graphic.setFont(o._headFontStyle);
    var titleWidth = graphic.textWidth(titleText);
    var titleLeft = left + (width - titleWidth) * 0.5;
+   var titleTop = top + padding + titleHeight;
    graphic.setFont(o._headFontStyle);
-   graphic.drawText(titleText, titleLeft, top + padding, '#55FFED');
+   graphic.drawText(titleText, titleLeft, titleTop, '#55FFED');
    //理财师排行榜
    var rankLeft = left + padding;
-   var rankTop = top + titleHeight + padding * 2;
-   var rankWidth = width - padding*2;
+   var rankTop = titleTop + padding * 2;
+   var rankWidth = width - padding * 2;
    var rankHeight = 40;
-   for(){
-      
+   var rankCount = 3;
+   var rankLastTop = 0;
+   for (var i = 1; i <= rankCount; i++) {
+      if (i == 1)
+         graphic.drawRectangle(rankLeft, rankTop, rankWidth, rankHeight, "#fff", 1);
+      else
+         graphic.drawRectangle(rankLeft, rankTop + rankHeight * (i - 1), rankWidth, rankHeight, "#fff", 1);
+
+      if (i == 3) {
+         rankLastTop = rankTop + rankHeight * (i - 1) + rankHeight;
+      }
    }
-   graphic.drawRectangle(rankLeft, rankTop, rankWidth, rankHeight, "#fff", 1);
-
-
-
-
+   //理财师实时表
+   var realtimeListTop = rankLastTop + padding;
+   var realtimeListHeight = height - rankLastTop - padding;
+   graphic.drawRectangle(rankLeft, realtimeListTop, rankWidth, realtimeListHeight, "#fff", 1);
+   //理财师实时表－标题
+   var tableTitleTexts = new Array('时间', '公司', '理财师', '理财师业绩', '注册/投资', '投资/赎回');
+   for(var j = 0 ; j<tableTitleTexts.length;j++){
+      var tableText = tableTitleTexts[j];
+      var tableTextWidth = graphic.textWidth(tableText);
+      //graphic.drawText();
+   }
+   
 }
