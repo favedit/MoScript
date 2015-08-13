f8430df16eac9807055c1c25b48ff4edd960d1b9
zyj2575 <zhaoyaoji2575@163.com>
2015-08-13 17:55:16 +0800

    修正 负数的时候 显示格式 左右对齐 数度变小向下滚动
    Change-Id: I47b196156e91776f792414c931eb1f61a975c44d

--- source/7.3.02-eai-logic-statistics/FEaiStatisticsLabel.js
+++ source/7.3.02-eai-logic-statistics/FEaiStatisticsLabel.js
@@ -13,6 +13,10 @@ MO.FEaiStatisticsLabel = function FEaiStatisticsLabel(o){
    o._originValue     = '0';
    // @attribute
    o._startTick       = 0;
+   o._textFontW       = "0";
+   o._unitFontW       = "0";
+   o._negative        = "0";
+   o._origin          = "0";
    o._rolling         = MO.Class.register(o, new MO.AGetSet('_rolling'), false);
    o._rollingDuration = MO.Class.register(o, new MO.AGetSet('_rollingDuration'), 1000);
    o._rollingPages    = null;
@@ -47,55 +51,87 @@ MO.FEaiStatisticsLabel_onPaintLabel = function FEaiStatisticsLabel_onPaintLabel(
    // 设置字体
    var textFont = o._foreFont;
    var unitFont = o._backFont;
+
    graphic.setFont(textFont);
-   var baseX = rectangle.left;
+   var baseX;
+   var unitTextX;
+   if( o._alignCd == "right" ){
+       baseX =  rectangle.left;
+       unitTextX = baseX + 4;
+   }else{
+       baseX =  rectangle.right() - o._unitFontW + (o._textFontW - o._unitFontW) - 65;
+       unitTextX = baseX ;
+   }
    var baseY = rectangle.top + rectangle.height;
-   var unitTextX = baseX + 4;
-   var unitTextY = baseY - 4;
+   var unitTextY = baseY - 2;
    var drawedText = '';
    var passedTick = MO.Timer.current() - o._startTick;
    if (passedTick > o._rollingDuration || o._noRolling) {
       passedTick = o._rollingDuration;
       o._rolling = false;
    }
+   
    for (var i = 0; i < o._value.length; i++) {
-      var passedValue = o._rollingPages.get(i) * (passedTick / o._rollingDuration);
-      var numString = (parseInt(o._originValue.charAt(i)) + parseInt(passedValue)).toString();
-      var currentNum = parseInt(numString.charAt(numString.length - 1));
-      var nextNum = currentNum == 9 ? 0 : currentNum + 1;
-      var rate = passedValue - parseInt(passedValue);
-      graphic.setFont(textFont);
-      var drawedTextWidth = graphic.textWidth(drawedText);
-      var textColor = '';
-      if (i < o._originValue.length - 8) {
-         textColor = '#FFD926';
-      }else if (i < o._originValue.length - 4) {
-         textColor = '#FF7200';
-      }else if (i < o._originValue.length) {
-         textColor = '#FD0000';
-      }
-      graphic.drawText(currentNum, baseX + drawedTextWidth, baseY - 38 * rate, textColor);
-      graphic.drawText(nextNum, baseX + drawedTextWidth, baseY + 38 - 38 * rate, textColor);
-      drawedText += currentNum;
-      if (i == o._originValue.length - 9) {
+         var passedValue = o._rollingPages.get(i) * (passedTick / o._rollingDuration);
+         var numString = (parseInt(o._originValue.charAt(i)) + parseInt(passedValue)).toString();
+         var currentNum = parseInt(numString.charAt(numString.length - 1));
+         var nextNum = currentNum == 9 ? 0 : currentNum + 1;
+         var prevNum = currentNum == 0 ? 9 : currentNum - 1;
+         var reg = /^[0-9]+$/;
+         var rate = passedValue - parseInt(passedValue);
+         graphic.setFont(textFont);
+         var drawedTextWidth;
+         var textColor = '';
+         if (i < o._originValue.length - 8) {
+            textColor = '#FFD926';
+         }else if (i < o._originValue.length - 4) {
+            textColor = '#FF7200';
+         }else if (i < o._originValue.length) {
+            textColor = '#FD0000';
+         }
+         
+         if( !reg.test(o._negative) ){
+            var negativeColor ="";
+            if ( o._negative.length <= 5) {
+               negativeColor = '#FD0000';
+            }else if ( o._negative.length <= 9) {
+               negativeColor = '#FF7200';
+            }else if ( o._negative.length <= 13) {
+               negativeColor = '#FFD926';
+            }
+            graphic.setFont(textFont);
+            graphic.drawText("-", baseX - 10, baseY , negativeColor);
+         }
+
          drawedTextWidth = graphic.textWidth(drawedText);
-         graphic.setFont(unitFont);
-         graphic.drawText('亿', unitTextX + drawedTextWidth, unitTextY, '#00B5F6');
-         drawedText += '亿';
-      }else if (i == o._originValue.length - 5) {
-         drawedTextWidth = graphic.textWidth(drawedText);
-         graphic.setFont(unitFont);
-         graphic.drawText('万', unitTextX + drawedTextWidth, unitTextY, '#00B5F6');
-         drawedText += '万';
-      }else if (i == o._originValue.length -1) {
-         drawedTextWidth = graphic.textWidth(drawedText);
-         graphic.setFont(unitFont);
-         graphic.drawText('元', unitTextX + drawedTextWidth, unitTextY, '#00B5F6');
-         drawedText += '元';
-      }
+         o._textFontW = drawedTextWidth;
+         // 字宽 ;  
+         graphic.drawText(prevNum, baseX + drawedTextWidth, baseY - 38 - 38 * rate, textColor);
+         graphic.drawText(currentNum, baseX + drawedTextWidth, baseY - 38 * rate, textColor);
+         graphic.drawText(nextNum, baseX + drawedTextWidth, baseY + 38 - 38 * rate, textColor);
+         drawedText += currentNum;
+         if (i == o._originValue.length - 9) {
+            drawedTextWidth = graphic.textWidth(drawedText);
+            graphic.setFont(unitFont);
+            graphic.drawText('亿', unitTextX + drawedTextWidth, unitTextY, '#00B5F6');
+            drawedText += '亿';
+         }else if (i == o._originValue.length - 5) {
+            drawedTextWidth = graphic.textWidth(drawedText);
+            graphic.setFont(unitFont);
+            graphic.drawText('万', unitTextX + drawedTextWidth, unitTextY, '#00B5F6');
+            drawedText += '万';
+         }else if (i == o._originValue.length -1) {
+            drawedTextWidth = graphic.textWidth(drawedText);
+            graphic.setFont(unitFont);
+            graphic.drawText('元', unitTextX + drawedTextWidth, unitTextY, '#00B5F6');
+            drawedText += '元';
+            o._unitFontW = drawedTextWidth;
+         }
+      
    }
    if (o._rolling == false) {
       o._originValue = o._value;
+      o._origin = o._negative;
       o._rollingPages.clear();
    }
 
@@ -108,18 +144,25 @@ MO.FEaiStatisticsLabel_onPaintLabel = function FEaiStatisticsLabel_onPaintLabel(
 //==========================================================
 MO.FEaiStatisticsLabel_setValue = function FEaiStatisticsLabel_setValue(value) {
    var o = this;
+   var fetch;
+   o._negative = value;
    if (o._value == value) {
       return;
    }
-
    if (o._rolling) {
       o._originValue = o._value;
+      o._origin = o._negative;
    }
-
+   o._negative = value;
+   fetch = value - o._origin;
+   var value = Math.abs(value).toString();
    o._value = value;
+   
+   console.log(o._originValue +"-"+ o._origin +"-"+value);
    //计算实际需要转过的页数
    var originValue = o._originValue;
    var lengthDiff = value.length - originValue.length;
+
    while (lengthDiff > 0) {
       originValue = '0' + originValue;
       lengthDiff--;
@@ -127,13 +170,21 @@ MO.FEaiStatisticsLabel_setValue = function FEaiStatisticsLabel_setValue(value) {
    o._originValue = originValue;
    o._rollingPages.clear();
    o._rollingPages._length = value.length;
-   for (var i = 0; i < value.length; i++) {
-      //var pages = parseInt(value.substring(0, i + 1)) - parseInt(originValue.substring(0, i + 1));
-      var pages = parseInt(value.substring(i, i + 1)) - parseInt(originValue.substring(i, i + 1));
-      pages = pages < 0 ? pages + 10 : pages;
-      o._rollingPages.set(i, pages);
-   }
 
+   if( fetch < 0  ){
+      for (var i = 0; i < value.length; i++) {
+         var pages = parseInt(value.substring(i, i + 1)) - parseInt(originValue.substring(i, i + 1));
+         pages = pages > 0 ? pages - 10 : pages;
+         o._rollingPages.set(i, pages);
+       }
+   }else{
+      for (var i = 0; i < value.length; i++) {
+            //var pages = parseInt(value.substring(0, i + 1)) - parseInt(originValue.substring(0, i + 1));
+            var pages = parseInt(value.substring(i, i + 1)) - parseInt(originValue.substring(i, i + 1));
+            pages = pages < 0 ? pages + 10 : pages;
+            o._rollingPages.set(i, pages);
+      }
+   }
    o._startTick = MO.Timer.current();
    o._rolling = true;
 }
