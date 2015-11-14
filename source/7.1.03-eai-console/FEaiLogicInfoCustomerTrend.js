7338bba757f6fc05b055ed7469f96a3232a18bb7
favedit <favedit@hotmail.com>
2015-11-14 17:12:53 +0800

    消息定义修正。
    Change-Id: Ideeaa69ea064358e4e528e223c40c5688c660093

--- source/7.1.03-eai-console/FEaiLogicInfoCustomerTrend.js
+++ source/7.1.03-eai-console/FEaiLogicInfoCustomerTrend.js
@@ -8,13 +8,19 @@
 MO.FEaiLogicInfoCustomerTrend = function FEaiLogicInfoCustomerTrend(o){
    o = MO.Class.inherits(this, o, MO.FObject, MO.MPersistence);
    // @attribute
-   o._investmentTotal = MO.Class.register(o, [new MO.AGetter('_investmentTotal'), new MO.APersistence('_investmentTotal', MO.EDataType.Double)]);
-   o._customerTotal   = MO.Class.register(o, [new MO.AGetter('_customerTotal'), new MO.APersistence('_customerTotal', MO.EDataType.Uint32)]);
+   o._investmentTotal          = MO.Class.register(o, [new MO.AGetter('_investmentTotal'), new MO.APersistence('_investmentTotal', MO.EDataType.Double)]);
+   o._customerTotal            = MO.Class.register(o, [new MO.AGetter('_customerTotal'), new MO.APersistence('_customerTotal', MO.EDataType.Uint32)]);
    // @attribute
    o._yesterdayInvestmentTotal = MO.Class.register(o, [new MO.AGetter('_yesterdayInvestmentTotal'), new MO.APersistence('_yesterdayInvestmentTotal', MO.EDataType.Double)]);
    o._yesterdayCustomerTotal   = MO.Class.register(o, [new MO.AGetter('_yesterdayCustomerTotal'), new MO.APersistence('_yesterdayCustomerTotal', MO.EDataType.Uint32)]);
+   // @attribute
+   o._weekInvestmentTotal      = MO.Class.register(o, [new MO.AGetter('_weekInvestmentTotal'), new MO.APersistence('_weekInvestmentTotal', MO.EDataType.Double)]);
+   o._weekCustomerTotal        = MO.Class.register(o, [new MO.AGetter('_weekCustomerTotal'), new MO.APersistence('_weekCustomerTotal', MO.EDataType.Uint32)]);
+   // @attribute
+   o._monthInvestmentTotal     = MO.Class.register(o, [new MO.AGetter('_monthInvestmentTotal'), new MO.APersistence('_monthInvestmentTotal', MO.EDataType.Double)]);
+   o._monthCustomerTotal       = MO.Class.register(o, [new MO.AGetter('_monthCustomerTotal'), new MO.APersistence('_monthCustomerTotal', MO.EDataType.Uint32)]);
    //..........................................................
    // @attribute
-   o._units           = MO.Class.register(o, [new MO.AGetter('_units'), new MO.APersistence('_units', MO.EDataType.Objects, MO.FEaiLogicInfoCustomerTrendUnit)]);
+   o._units                    = MO.Class.register(o, [new MO.AGetter('_units'), new MO.APersistence('_units', MO.EDataType.Objects, MO.FEaiLogicInfoCustomerTrendUnit)]);
    return o;
 }
